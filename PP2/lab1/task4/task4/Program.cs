5207b3f17c9de7f72e9c1e6a8a0a39d7f503f06b
darkMarcus <47265380+darkMarcus@users.noreply.github.com>
2019-02-09 13:42:39 +0600

    Update Program.cs

--- PP2/lab1/task4/task4/Program.cs
+++ PP2/lab1/task4/task4/Program.cs
@@ -3,10 +3,19 @@
 namespace task4 {
     class MainClass {
         public static void Main(string[] args) {
-            int n = int.Parse(Console.ReadLine());
-            for(int i = 0; i <= n; i++) {
-                for(int j = 0; j < i; j++) {
-                    Console.Write("[*]");
+            // int n = int.Parse(Console.ReadLine());
+            // for(int i = 0; i <= n; i++) {
+            //     for(int j = 0; j < i; j++) {
+            //         Console.Write("[*]");
+            //     }
+            //     Console.Write("\n");
+            // }
+            int[ , ] arr = new int[n, n];
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                    if (i >= j) {
+                        Console.Write("[*]");
+                    }
                 }
                 Console.Write("\n");
             }
