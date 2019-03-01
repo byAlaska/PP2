a4a34bdf92a7331a0349f880a552e316bf37f0ed
darkMarcus <askardatkambek@gmail.com>
2019-03-01 23:42:09 +0600

    lab4

---
+++ PP2/lab4/task1/task1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+namespace task1 {
+
+    [Serializable]
+    class Complex {
+        public string a;
+        public string b;
+        public Complex(string a, string b) {
+            this.a = a;
+            this.b = b;
+        }
+        public string s;
+        public void Comp() {
+            s = " " + a + " + " + b + "i";
+        }
+        override
+            public string ToString() {
+            return s;
+        }
+    }
+    class MainClass {
+        public static void Ser() {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            Complex complex = new Complex(a, b);
+            complex.Comp();
+            FileStream fs = new FileStream("Complex.txt", FileMode.Create, FileAccess.Write);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs, complex);
+            fs.Close();
+        }
+
+        public static void DeSer() {
+            FileStream fs = new FileStream("Complex.txt", FileMode.Open, FileAccess.Read);
+            BinaryFormatter bf = new BinaryFormatter();
+            Complex complex = bf.Deserialize(fs) as Complex;
+            Console.WriteLine(complex);
+        }
+
+        public static void Main(string[] args) {
+            Ser();
+            DeSer();
+        }
+    }
+}
---
+++ PP2/lab4/task2/task2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace task2 {
+    [Serializable]
+    class Mark {
+        public int points;
+        public Mark(int points) {
+            this.points = points;
+        }
+        public string GetLetter() {
+            if (points < 50 && points>-1) {
+                return "F";
+            }else if(points>49 && points< 60) {
+                return "C-";
+            }else if(points>59 && points < 70) {
+                return "C";
+            }else if(points>69 && points < 75) {
+                return "C+";
+            }else if(points>74 && points < 80) {
+                return "B-";
+            }else if(points>79 && points < 85) {
+                return "B";
+            }else if(points>84 && points < 90) {
+                return "B+";
+            }else if(points>89 && points < 95) {
+                return "A-";
+            } else {
+                return "A";
+            }
+        }
+        override
+            public string ToString() {
+            return GetLetter();
+        }
+    }
+    class MainClass {
+        public static void Ser() {
+            List<Mark> marks = new List<Mark>();
+            for (int i = 60; i < 96; i++) {
+                marks.Add(new Mark(i));
+            }
+            FileStream fs = new FileStream("Mark.txt", FileMode.Create, FileAccess.Write);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs, marks);
+            fs.Close();
+        }
+
+        public static void DeSer() {
+            List<Mark> marks = new List<Mark>();
+            FileStream fs = new FileStream("Mark.txt", FileMode.Open, FileAccess.Read);
+            BinaryFormatter bf = new BinaryFormatter();
+            marks = bf.Deserialize(fs) as List<Mark>;
+            for(int i = 0; i < marks.Count; i++) {
+                Console.WriteLine(marks[i]);
+            }
+            fs.Close();
+        }
+        public static void Main(string[] args) {
+            Ser();
+            DeSer();
+        }
+    }
+}
